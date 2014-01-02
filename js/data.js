bf1d58be9a5f0831ace8dbed5148859b0e740e72
Niki Eskola <neskola@gmail.com>
2014-01-02 20:43:55 +0200

    added external js files

---
+++ js/data.js
@@ -0,0 +1,16 @@
+function CalendarDataCtrl($scope) {
+
+  $scope.entries = [
+    {text:'workshop'},
+    {text:'next event'},
+	{text:'further event'}];
+
+};
+
+function WeatherDataCtrl($scope) {
+
+	$scope.entries = [
+	{text:"cloudy"},
+	{text:"sunny"},
+	{text:"rainy"}];
+};
