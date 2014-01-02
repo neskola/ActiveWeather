243da793adff3055d8b25b6fb8070b2ec9fd78c5
Niki Eskola <neskola@gmail.com>
2014-01-02 22:30:08 +0200

    testing ajax request to FMIs service

--- js/data.js
+++ js/data.js
@@ -9,8 +9,20 @@ function CalendarDataCtrl($scope) {
 
 function WeatherDataCtrl($scope) {
 
+	var place = 'Helsinki';
+
+	$.ajax({
+		type: "GET",
+		url: "http://data.fmi.fi/fmi-apikey/8a861995-5bad-4fea-85e8-7cccd6860bb2/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&place=helsinki",
+		dataType: "xml",
+		success: function(xml) {
+			// get data from xml file
+		}
+	});
+	
 	$scope.entries = [
 	{text:"cloudy", temp:'5'},
 	{text:"sunny", temp:'3'},
 	{text:"rainy", temp:'4'}];
 };
+
