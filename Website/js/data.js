6e72f5d93fa17951c0df1b1d4746eab4799f9f37
Niki Eskola <neskola@gmail.com>
2014-01-19 15:21:16 +0200

    json is loaded ok - but ctrller returns empty object

--- Website/js/data.js
+++ Website/js/data.js
@@ -7,30 +7,31 @@ function CalendarDataCtrl($scope) {
 
 };
 
-function WeatherDataCtrl($scope) {
+function WeatherDataCtrl($scope, $http) {
 
-	var place = 'Helsinki';
-
-	$.ajax({
-		type: "GET",
-		url: "http://data.fmi.fi/fmi-apikey/8a861995-5bad-4fea-85e8-7cccd6860bb2/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&place=helsinki",
-		dataType: "xml",
-		success: function(xml) {
-			$("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Ok!</strong> Weather data refresh was succesfull</div>").appendTo("#status");
-			// get data from xml file
-		},
-		error: function(xml) {
-			$("<div class='alert alert-warning alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Warning!</strong> Weather data refresh was NOT succesfull</div>").appendTo("#status");
-			// failed
-		}
-	});
-	
-	$scope.entries = [
-	{text:"cloudy", temp:'5'},
-	{text:"sunny", temp:'3'},
-	{text:"rainy", temp:'4'}];
-	
-	
-
+    var place = 'Helsinki';
+    var result = getWeatherData(place, $http).then(function () {
+        $scope.entries = result;
+    });
+    
+    alert(JSON.stringify($scope.entries));
 };
 
+function getWeatherData($place, $http) {
+    var promise = $http({
+        method : 'GET',
+        url : './python/' + $place + '.json'
+    }).success(function (data, status, headers, config) {
+        $("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Ok!</strong> Weather data refresh was succesfull</div>").appendTo("#status");
+        console.log('Done');
+        alert(JSON.stringify(data));
+        entries = data;
+        return entries;
+    }).error(function (data, status, headers, config) {
+        $("<div class='alert alert-warning alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Warning!</strong> Weather data refresh was NOT succesfull</div>").appendTo("#status");                
+        entries = data;
+        return entries;
+    });
+
+    return promise; 
+}; 
