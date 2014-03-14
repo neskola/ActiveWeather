c8eb6f1962b5d2b98c5c078a3ac3ff4406dfd19c
Niki Eskola <neskola@gmail.com>
2014-03-14 11:30:47 +0200

    added backup functionality with zip compress

--- Website/python/firebase.py
+++ Website/python/firebase.py
@@ -3,34 +3,71 @@
 import sys, getopt, json, ssl
 import pycurl
 import StringIO
+import datetime, zipfile
+
+__PUSH = 1
+__BACKUP = 2
+__BACKUP_AND_ZIP = 3
+
+
+def usage():
+	print ("firebase.py -b|-u|-z --file <json file> --url <firebase url>")
+	print ("-b backup data from <firebase_url> to <json file>")
+	print ("-u upload data from <json file> to <firebase_url>")
+	print ("-z backup and compress (zip) data from <firebase_url> to <json_file>.zip")
+	return
 
 # for testing only
 def main(argv): 
 
-	global url, inputfile
+	global url, fileref, operation
 	
-	inputfile = ""
 	try:
-		opts, args = getopt.getopt(argv,"hf:u:", ["file=", "url="])
+		opts, args = getopt.getopt(argv,"hbzu", ["file=", "url="])
 	except getopt.GetoptError:
-		print ("firebase.py -f|--file <json file> -u|--url <firebase url>")
+		usage()
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
-			print ("firebase.py -f|--file <json file> -u|--url <firebase url>")
+			usage()
 			sys.exit()
-		elif opt in ("-f", "--file"):			
-			inputfile = arg
-			print ("Using " + inputfile + " as input file.")
-		elif opt in ("-u", "--url"):			
+		elif opt in ("--file"):			
+			fileref = arg
+		elif opt in ("--url"):			
 			url = arg
+		if opt in ("-b"):
+			operation = __BACKUP
+		if opt in ("-p"):
+			operation = __PUSH
+		if opt in ("-z"):
+			operation = __BACKUP_AND_ZIP
 
-	if inputfile != '' and url != '':
-		jsonfile = open(inputfile)
+	if not fileref and not url:
+		usage()
+		sys.exit(1)
+	elif operation == __PUSH:
+		print ("Uploading file " + fileref + " to firebase url " + url)
+		jsonfile = open(fileref)
 		jsondata = json.load(jsonfile)
 		print json.dumps(jsondata)
 		jsonfile.close()
 		curlPut(url, json.dumps(jsondata))
+	elif operation >= __BACKUP:
+		print ("Creating backup from firebase url " + url + " to file " + fileref)
+		response = curlQuery(url + "/.json")
+		jsondata = json.loads(response) 
+		with open(fileref, 'w') as outfile:
+			json.dump(jsondata, outfile, sort_keys = True, indent = 4, ensure_ascii=True)
+		outfile.close()
+		if operation == __BACKUP_AND_ZIP:
+			datestring = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+			zipfilename = fileref + "." + datestring + ".zip"
+			print ("Creating archive " + zipfilename)
+			zf = zipfile.ZipFile(zipfilename, mode='w')
+			try:
+				zf.write(fileref)
+			finally:
+				zf.close()
 
 # Firebase API functions, no fancy realtime stuff just plain PUSH / GET functions
 
